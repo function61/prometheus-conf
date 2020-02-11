8eeac8bea303a1ff15f57e962847b4f2a65e6119
Joonas Loppi <joonas@joonas.fi>
2020-02-11 10:19:54 +0200

    more info for debugging flaky backups

--- bin/prom-snapshot-as-tar.sh
+++ bin/prom-snapshot-as-tar.sh
@@ -10,7 +10,13 @@ function bootstrapDir {
 
 function takeBackup {
 	# need dummy --post-data to make a POST request
-	wget -O /prometheus/snapshots/latest.json --post-data '' http://localhost:9090/api/v1/admin/tsdb/snapshot?skip_head=false
+	# server response (goes properly to stderr) because this seems to fail sometimes
+	wget \
+		--server-response \
+		--quiet \
+		--output-document /prometheus/snapshots/latest.json \
+		--post-data '' \
+		http://localhost:9090/api/v1/admin/tsdb/snapshot?skip_head=false
 }
 
 # streams the .tar into stdout so the caller can in space-efficient way process it further
@@ -23,6 +29,9 @@ bootstrapDir
 cleanup
 
 takeBackup
+
+# testing if this helps with flaky backups where tar fails for unknown reason
+sleep 2
 
 streamTar
 
